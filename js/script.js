3609a85e1fd07d96510884910b98f7bc651b229b
Eduardo Lima Vital <eduardolimavital@gmail.com>
2023-09-21 02:37:57 -0300

    modified:   css/style.css 	new file:   img/background.jpg 	modified:   index.html 	modified:   js/script.js

--- js/script.js
+++ js/script.js
@@ -1,24 +1,38 @@
-let listarAtivo = false;
+let listarAtivo = true;
 
 function toggleListarProdutos() {
-    listarAtivo = !listarAtivo;
-    document.getElementById('botao-toggle').innerHTML = listarAtivo ? 'Ocultar' : 'Exibir';
+    document.getElementById('buscar_produto_id').value = '';
+    //document.getElementById('botao-toggle').innerHTML = listarAtivo ? 'Resetar Busca de Produtos' : 'Exibir Todos os Produtos';
+
     if (listarAtivo) {
         listarProdutos();
     } else {
         document.getElementById('lista-produtos').innerHTML = '';
     }
+
+    //listarAtivo = !listarAtivo;
 }
 
+
 function listarProdutos() {
-    if (!listarAtivo) return;
+    listarAtivo = true;
+    document.getElementById('lista-produtos').innerHTML = '';
 
     fetch('http://127.0.0.1:5000/produtos')
         .then(response => response.json())
         .then(data => data.produtos.forEach(produto => {
-            document.getElementById('lista-produtos').innerHTML += '<tr><td>' + produto.id + '</td><td>' + produto.data_de_cadastro + '</td><td>' + produto.nome_produto + '</td><td>' + produto.preco_produto + '</td><td>' + produto.quantidade_produto + '</td></tr>';
+            document.getElementById('lista-produtos').innerHTML +=
+                `<tr>
+                <td>${produto.id}</td>
+                <td>${produto.data_de_cadastro}</td>
+                <td>${produto.nome_produto}</td>
+                <td>R$ ${parseFloat(produto.preco_produto).toFixed(2)}</td>
+                <td>${produto.quantidade_produto}</td>
+                <td><button onclick="deletarProduto(${produto.id})" class="btn btn-danger btn-sm">Deletar</button></td>
+            </tr>`;
         }));
 }
+
 
 function cadastrarProduto(event) {
     event.preventDefault();
@@ -47,8 +61,7 @@ function cadastrarProduto(event) {
 }
 
 
-function deletarProduto() {
-    const id = document.getElementById('produto_id').value;
+function deletarProduto_completo(id) {
     if (id) {
         fetch(`http://127.0.0.1:5000/produtos/${id}`, {
             method: 'DELETE',
@@ -56,7 +69,6 @@ function deletarProduto() {
             .then(response => response.json())
             .then(data => {
                 console.log(data);
-                // Atualizar a lista de produtos ou recarregar a página aqui, se necessário
                 window.location.reload();
             })
             .catch(error => console.error('Erro:', error));
@@ -74,6 +86,61 @@ function exibirProduto(id) {
             document.getElementById('produto-detalhes').innerHTML = 'ID: ' + data.id + '<br>Nome: ' + data.nome_produto + '<br>Preço: ' + data.preco_produto + '<br>Quantidade: ' + data.quantidade_produto;
         });
 }
+
+function buscarProdutoPorID() {
+    const id = document.getElementById('buscar_produto_id').value;
+    if (id) {
+        fetch(`http://127.0.0.1:5000/produtos/${id}`, {
+            method: 'GET',
+        })
+            .then(response => response.json())
+            .then(data => {
+                console.log(data);
+                const tabela = document.getElementById('lista-produtos');
+                if (data.error || !data.id) { // Verifique se a resposta contém um erro ou se o ID do produto está ausente
+                    tabela.innerHTML = `<tr>
+                        <td colspan="6">Produto não encontrado</td>
+                    </tr>`;
+                } else {
+                    tabela.innerHTML = `<tr>
+                        <td>${data.id}</td>
+                        <td>${data.data_de_cadastro}</td>
+                        <td>${data.nome_produto}</td>
+                        <td>${data.preco_produto}</td>
+                        <td>${data.quantidade_produto}</td>
+                    </tr>`;
+                }
+            })
+            .catch(error => {
+                console.error('Erro:', error);
+                const tabela = document.getElementById('lista-produtos');
+                tabela.innerHTML = `<tr>
+                    <td colspan="5">Produto não encontrado</td>
+                </tr>`;
+            });
+    } else {
+        console.error('ID do produto não especificado.');
+    }
+}
+
+function deletarProduto(id) {
+    if (id) {
+        fetch(`http://127.0.0.1:5000/produtos/${id}`, {
+            method: 'DELETE',
+        })
+            .then(response => response.json())
+            .then(data => {
+                console.log(data);
+                listarProdutos(); // Chame listarProdutos novamente para atualizar a lista de produtos
+            })
+            .catch(error => console.error('Erro:', error));
+    } else {
+        console.error('ID do produto não especificado.');
+    }
+}
+
+
+window.onload = listarProdutos;
 
 
 
