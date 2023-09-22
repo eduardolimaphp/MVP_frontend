39b04e958cab90fd0793440312e59e537a97cbae
Eduardo Lima Vital <eduardolimavital@gmail.com>
2023-09-22 03:43:41 -0300

    Melhoria de funcionalidades

--- js/script.js
+++ js/script.js
@@ -1,18 +1,10 @@
 let listarAtivo = true;
 
 function toggleListarProdutos() {
-    document.getElementById('buscar_produto_id').value = '';
-    //document.getElementById('botao-toggle').innerHTML = listarAtivo ? 'Resetar Busca de Produtos' : 'Exibir Todos os Produtos';
+    document.getElementById('buscar_produto_texto').value = '';
 
-    if (listarAtivo) {
-        listarProdutos();
-    } else {
-        document.getElementById('lista-produtos').innerHTML = '';
-    }
-
-    //listarAtivo = !listarAtivo;
+    listarProdutos();
 }
-
 
 function listarProdutos() {
     listarAtivo = true;
@@ -20,18 +12,15 @@ function listarProdutos() {
 
     fetch('http://127.0.0.1:5000/produtos')
         .then(response => response.json())
-        .then(data => data.produtos.forEach(produto => {
-            document.getElementById('lista-produtos').innerHTML +=
-                `<tr>
-                <td>${produto.id}</td>
-                <td>${produto.data_de_cadastro}</td>
-                <td>${produto.nome_produto}</td>
-                <td>R$ ${parseFloat(produto.preco_produto).toFixed(2)}</td>
-                <td>${produto.quantidade_produto}</td>
-                <td><button onclick="prepararVenda(${produto.id}, ${produto.quantidade_produto})" data-bs-toggle="modal" data-bs-target="#vendaModal" class="btn btn-success btn-sm">Realizar Venda</button>
-                <button onclick="deletarProduto_completo(${produto.id})" class="btn btn-danger btn-sm">Excluir Produto</button></td>
-            </tr>`;
-        }));
+        .then(data => {
+            let produtos = '';
+
+            data.produtos.forEach(produto => {
+                 produtos += normalizaLinhaProduto(produto)
+            })
+
+            document.getElementById('lista-produtos').innerHTML = produtos;
+        });
 }
 
 
@@ -44,54 +33,82 @@ function prepararVenda(id, quantidade) {
     document.getElementById('quantidade_venda').max = quantidade;
 }
 
-function confirmarVenda() {
-    if (produtoVendaId) {
-        const quantidadeVenda = parseInt(document.getElementById('quantidade_venda').value);
-        if (quantidadeVenda > 0 && quantidadeVenda <= quantidadeMaxima) {
-            // Aqui, você pode fazer uma chamada API para confirmar a venda.
-            // Para o propósito deste exemplo, apenas decrementamos a quantidade do produto.
 
-            // Você pode substituir esta parte por uma chamada API real
-            console.log(`Vendeu ${quantidadeVenda} do produto de ID: ${produtoVendaId}`);
-            
-            // Atualiza a quantidade no servidor
-            const novaQuantidade = quantidadeMaxima - quantidadeVenda;
-            fetch(`http://127.0.0.1:5000/produtos/${produtoVendaId}`, {
-                method: 'PUT',  // Assuma que PUT é o método para atualizar um produto
+function confirmarVenda() {
+    const quantidadeVenda = parseInt(document.getElementById('quantidade_venda').value);
+
+    if (!produtoVendaId) {
+        showMessageModal('Produto não selecionado para venda.');
+        return;
+    }
+
+    if (quantidadeVenda <= 0 || quantidadeVenda > quantidadeMaxima) {
+        showMessageModal('Erro: A quantidade que você está tentando vender é inválida!');
+        return;
+    }
+
+    fetch(`http://127.0.0.1:5000/produtos/${produtoVendaId}`)
+        .then(response => response.json())
+        .then(produto => {
+            if (produto.produtos?.length == 0) {
+                showMessageModal('Ocorreu um erro ao realizar venda');
+                return;
+            }
+
+            const novaQuantidade = produto.produtos[0].quantidade_produto - quantidadeVenda;
+
+            if (novaQuantidade < 0) {
+                showMessageModal('Erro: A quantidade que você está tentando vender é maior que a quantidade disponível em estoque!');
+                return;
+            }
+
+            // Se a nova quantidade é 0 ou a quantidade de venda 
+            // é igual à quantidade em estoque, deleta o produto
+            if (novaQuantidade === 0) {
+                return fetch(`http://127.0.0.1:5000/produtos/${produtoVendaId}`, {
+                    method: 'DELETE',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify({ quantidade_produto: novaQuantidade })
+                })
+                    .then(response => {
+                        if (!response.ok) throw new Error('Erro ao deletar o produto');
+                        showMessageModal('Venda realizada! O produto foi deletado pois acabou.');
+                    });
+            }
+
+            return fetch(`http://127.0.0.1:5000/produtos/${produtoVendaId}`, {
+                method: 'PUT',
                 headers: {
                     'Content-Type': 'application/json'
                 },
-                body: JSON.stringify({
-                    quantidade_produto: novaQuantidade
-                })
+                body: JSON.stringify({ quantidade_produto: novaQuantidade })
             })
-            .then(response => response.json())
-            .then(data => {
-                if (data.success) {
-                    console.log(`Vendeu ${quantidadeVenda} do produto de ID: ${produtoVendaId}. Nova quantidade: ${novaQuantidade}`);
-                } else {
-                    console.error('Erro ao vender o produto:', data.message);
-                }
-            })
-            .catch(error => console.error('Erro ao fazer a chamada API:', error));
-
-            
-            // Fim da parte substituível
-
-            // Fechar modal
-            var modalVenda = new bootstrap.Modal(document.getElementById('vendaModal'));
+                .then(response => {
+                    if (!response.ok) throw new Error('Erro ao atualizar o produto');
+                    showMessageModal(`Venda de ${quantidadeVenda} unidades do produto ID: ${produtoVendaId} realizada! Nova quantidade: ${novaQuantidade}`);
+                });
+        })
+        .catch(error => {
+            console.error('Erro ao vender o produto:', error);
+            showMessageModal('Erro ao realizar a venda. Por favor, tente novamente.');
+        })
+        .finally(() => {
+            const modalVenda = new bootstrap.Modal(document.getElementById('vendaModal'));
             modalVenda.hide();
-            
-            listarProdutos(); // Atualize a lista após a venda.
-        } else {
-            console.error('Quantidade de venda inválida.');
-        }
-    } else {
-        console.error('Produto não selecionado para venda.');
-    }
+            listarProdutos();
+        });
 }
 
-
+function showMessageModal(message) {
+    document.getElementById('messageModalBody').textContent = message;
+    const messageModal = new bootstrap.Modal(document.getElementById('messageModal'));
+    messageModal.show();
+    setTimeout(() => {
+        messageModal.hide();
+    }, 3000);
+}
 
 function cadastrarProduto(event) {
     event.preventDefault();
@@ -112,30 +129,28 @@ function cadastrarProduto(event) {
         body: formData,
     })
         .then(response => response.json())
+        .then(() => {
+            window.location.reload();
+        })
+        .catch(error => showMessageModal(error));
+}
+
+function deletarProduto_completo(id) {
+    if (!id) {
+        showMessageModal('ID do produto não especificado.');
+        return;
+    }
+
+    fetch(`http://127.0.0.1:5000/produtos/removeregistro/${id}`, {
+        method: 'DELETE',
+    })
+        .then(response => response.json())
         .then(data => {
             console.log(data);
             window.location.reload();
         })
-        .catch(error => console.log(error));
+        .catch(error => console.error('Erro:', error));
 }
-
-
-function deletarProduto_completo(id) {
-    if (id) {
-        fetch(`http://127.0.0.1:5000/produtos/removeregistro/${id}`, {
-            method: 'DELETE',
-        })
-            .then(response => response.json())
-            .then(data => {
-                console.log(data);
-                window.location.reload();
-            })
-            .catch(error => console.error('Erro:', error));
-    } else {
-        console.error('ID do produto não especificado.');
-    }
-}
-
 
 function exibirProduto(id) {
     fetch(`http://127.0.0.1:5000/produtos/${id}`)
@@ -146,71 +161,74 @@ function exibirProduto(id) {
         });
 }
 
-function confirmarVenda() {
-    if (produtoVendaId) {
-        const quantidadeVenda = parseInt(document.getElementById('quantidade_venda').value);
-        if (quantidadeVenda > 0 && quantidadeVenda <= quantidadeMaxima) {
-            // Calcula a nova quantidade após a venda
-            const novaQuantidade = quantidadeMaxima - quantidadeVenda;
-
-            // Criar FormData para armazenar os dados
-            const formData = new FormData();
-            formData.append('quantidade_produto', novaQuantidade);
-
-            // Atualiza a quantidade no servidor
-            fetch(`http://127.0.0.1:5000/produtos/${produtoVendaId}`, {
-                method: 'PUT',  // Assuma que PUT é o método para atualizar um produto
-                body: formData
-            })
-            .then(response => {
-                if (response.status === 200) {
-                    return response.json();
-                } else {
-                    throw new Error(`Error with status: ${response.status}`);
-                }
-            })
-            .then(data => {
-                console.log(`Vendeu ${quantidadeVenda} do produto de ID: ${produtoVendaId}. Nova quantidade: ${novaQuantidade}`);
-            })
-            .catch(error => {
-                console.error('Erro ao vender o produto:', error);
-            })
-            .finally(() => {
-                // Fechar modal
-                var modalVenda = new bootstrap.Modal(document.getElementById('vendaModal'));
-                modalVenda.hide();
-
-                listarProdutos(); // Atualize a lista após a venda.
-            });
-        } else {
-            console.error('Quantidade de venda inválida.');
-        }
-    } else {
-        console.error('Produto não selecionado para venda.');
-    }
-}
-
-
-
 function deletarProduto(id) {
-    if (id) {
-        fetch(`http://127.0.0.1:5000/produtos/${id}`, {
-            method: 'DELETE',
-        })
-            .then(response => response.json())
-            .then(data => {
-                console.log(data);
-                listarProdutos(); // Chame a função listarProdutos para atualizar a lista de produtos.
-            })
-            .catch(error => console.error('Erro:', error));
-    } else {
-        console.error('Não foi possível deletar o produto.');
+    if (!id) {
+        showMessageModal('Não foi possível deletar o produto.');
+        return
     }
+
+    fetch(`http://127.0.0.1:5000/produtos/${id}`, {
+        method: 'DELETE',
+    })
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            listarProdutos();
+        })
+        .catch(error => console.error('Erro:', error));
 }
 
+function buscarProduto() {
+    const busca = document.getElementById('buscar_produto_texto').value;
+    const tabela = document.getElementById('lista-produtos');
+    const errorMsgProdutoNaoEncontrado = `
+        <tr>
+            <td colspan="6">Produto não encontrado</td>
+        </tr>
+    `;
+
+    if (!busca) {
+        alert('ID do produto não especificado.');
+        return;
+    }
+
+    fetch(`http://127.0.0.1:5000/produtos/${busca}`, {
+        method: 'GET',
+    })
+        .then(response => response.json())
+        .then(produto => {
+            if (produto.length == 0) {
+                tabela.innerHTML = errorMsgProdutoNaoEncontrado;
+                return;
+            }
+            let produtos = ''
+            produto.produtos.forEach(produto => {
+                produtos += normalizaLinhaProduto(produto);
+            })
+            console.log(produtos)
+            tabela.innerHTML = produtos;
+
+            return;
+        })
+        .catch(error => {
+            console.error('Erro:', error);
+            tabela.innerHTML = errorMsgProdutoNaoEncontrado;
+        });
+
+}
+
+function normalizaLinhaProduto(produto) {
+    return `
+        <tr>
+            <td>${produto.id}</td>
+            <td>${produto.data_de_cadastro}</td>
+            <td>${produto.nome_produto}</td>
+            <td>R$ ${parseFloat(produto.preco_produto).toFixed(2)}</td>
+            <td>${produto.quantidade_produto}</td>
+            <td><button onclick="prepararVenda(${produto.id}, ${produto.quantidade_produto})" data-bs-toggle="modal" data-bs-target="#vendaModal" class="btn btn-success btn-sm">Realizar Venda</button>
+            <button onclick="deletarProduto_completo(${produto.id})" class="btn btn-danger btn-sm">Excluir Produto</button></td>
+        </tr>
+    `;
+}
 
 window.onload = listarProdutos;
-
-
-
-
